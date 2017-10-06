7d872fc84f63208173f57767d8fc398f065946fc
Szymon W <szymon.wojciechowski@sequencesh.pl>
2017-10-06 16:40:52 +0200

    change data that reducer received

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,13 +1,10 @@
 import * as types from '../actions/actionTypes';
 
+/** This reducer handles the LOAD_COURSES_SUCCESS action */
 export default function courseReducer(state = [], action) {
   switch(action.type) {
-    case types.CREATE_COURSE:
-      /*Here we copy deeply action.course and assign in object*/
-      debugger;
-      return [...state,
-        Object.assign({}, action.course)
-      ];
+    case types.LOAD_COURSES_SUCCESS:
+      return action.courses;
 
     default: 
       return state;  
